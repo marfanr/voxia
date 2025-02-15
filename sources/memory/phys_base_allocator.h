d07563df3007adf3a324cdfdebb1a2b8d0750a58
Mohammad Arfan <32129450+marfanr@users.noreply.github.com>
2025-02-15 17:59:53 +0700

    feat: update phys base allocator to  32bt bitmap

--- sources/memory/phys_base_allocator.h
+++ sources/memory/phys_base_allocator.h
@@ -4,11 +4,11 @@
 #include <libk/stivale2.h>
 #include <libk/type.h>
 
-void phys_base_allocator_install(
+void phys_base_allocator_install (
     struct stivale2_struct_tag_memmap *stivale_memmap);
-void *phys_base_alloc(uint64_t size);
-void phys_base_free(void *page, uint64_t length);
-
+void *phys_base_alloc (uint64_t size);
+void phys_base_free (void *page, uint64_t length);
+void pmm_log_usage ();
 extern uint64_t bitmap_size_;
 
 #endif // __MEMORY__ALLOCATOR_H__
