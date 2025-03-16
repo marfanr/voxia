c1bd5c7516788bd4682b38f6673f1f39b7c4a5f4
Mohammad Arfan <32129450+marfanr@users.noreply.github.com>
2025-03-16 11:08:37 +0700

    feat(adding a pci forge)

---
+++ sources/sys/ioforge/ioforge_pci.h
@@ -0,0 +1,34 @@
+#ifndef __SYS__IOFORGE__IOFORGE_PCI_H_
+#define __SYS__IOFORGE__IOFORGE_PCI_H_
+
+#include "./ioforge.h"
+
+struct IoForgePCIBar
+{
+    uint32_t address;
+    bool iospace;
+};
+
+struct IoForgePCI
+{
+    struct IoForgeService service;
+    uint16_t vendor_id;
+    uint16_t device_id;
+    uint16_t command;
+    uint16_t status;
+    uint8_t subclass;
+    uint8_t class;
+    uint8_t prog_if;
+    uint8_t revision_id;
+    uint8_t header_type;
+    uint8_t interrupt_line;
+    uint8_t interrupt_pin;
+    uint8_t min_grant;
+    uint8_t max_latency;
+    uint8_t bus;
+    uint8_t device;
+    uint8_t function;
+    struct IoForgePCIBar bar[5];
+};
+
+#endif // __SYS__IOFORGE__IOFORGE_PCI_H_
