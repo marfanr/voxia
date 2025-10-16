37ac2e98b2e0076e8da1794f2dd9aa3a90aa4ac4
marfanr <mohammadarfan45@gmail.com>
2025-10-16 14:47:16 +0700

    feat(block): refactor block device management and improve structure

--- kernel/hal/block/block.c
+++ kernel/hal/block/block.c
@@ -2,52 +2,44 @@
 #include <libk/hash.h>
 #include <libk/serial.h>
 #include <libk/str.h>
-#include <memory/buddy_allocator.h>
 #include <memory/memory_utils.h>
 #include <memory/phys_base_allocator.h>
+#include <memory/slab.h>
 
-#define BLOCK_DEVICE_SIZE 32
-
-static struct block_device **g__block_devices_ = 0;
-struct buddy_allocator *block_allocator = 0;
+static struct block_device *g__block_devices_root_ = 0;
+struct slab_cache          *block_device_cache     = 0;
 
 void
-block_install ()
+block_install()
 {
-    size_t estimated_size = (1024 * 1024 * 10);
-    block_allocator = buddy_allocator_install (
-        (void *)(phys_base_alloc (1 + estimated_size / 4096)),
-        (1 + estimated_size / 4096) * 4096);
-
-    g__block_devices_ = (struct block_device **)buddy_alloc (
-        block_allocator, sizeof (uintptr_t) * BLOCK_DEVICE_SIZE);
-    memset (g__block_devices_, 0, sizeof (uintptr_t) * BLOCK_DEVICE_SIZE);
-
-    serial_trace ("block device installed\n");
+    slab_cache_create(&block_device_cache, "block_device", sizeof(struct block_device), 0, 0);
 }
 
 void
-block_register_device (const char *name, block_device_operations_t *ops,
-                       void *identifier)
+block_register_device(const char *name, block_device_operations_t *ops, void *identifier)
 {
-    struct block_device *device = (struct block_device *)buddy_alloc (
-        block_allocator, sizeof (struct block_device));
-    device->name = name;
-    device->ops = ops;
-    device->identifier = identifier;
-    device->used = 0;
-    int index = hash (name, BLOCK_DEVICE_SIZE);
-    g__block_devices_[index] = device;
-    serial_trace ("\n [bLOCK] block device registered %s\n", name);
+    struct block_device *device = (struct block_device *)slab_alloc(block_device_cache);
+    strcpy((char *)device->name, name);
+    device->ops            = ops;
+    device->identifier     = identifier;
+    device->bar            = 0;
+    device->used           = 0;
+    device->next           = g__block_devices_root_;
+    g__block_devices_root_ = device;
+    LOG_INFO("BLOCK", "[bLOCK] block device registered %s", name);
 }
 
-struct block_device *
-block_get_device (const char *name)
+block_device *
+block_get_device(const char *name)
 {
-    int index = hash (name, BLOCK_DEVICE_SIZE);
-    struct block_device *block = g__block_devices_[index];
-    serial_trace ("block found\n");
-    if (block != 0)
-        return block;
+    block_device *device = g__block_devices_root_;
+    while (device)
+    {
+        if (strncmp(device->name, name, 64) == 0)
+        {
+            return device;
+        }
+        device = device->next;
+    }
     return 0;
 }
--- kernel/hal/block/block.h
+++ kernel/hal/block/block.h
@@ -3,24 +3,41 @@
 
 #include <libk/type.h>
 
+typedef enum
+{
+    FMODE_R = 1UL << 0,
+    FMODE_W = 1UL << 1,
+    FMODE_E = 1UL << 2
+} fmode_t;
+
+typedef enum
+{
+    OPEN_SUCCESS = 0,
+    OPEN_FAILED  = 1
+} open_response_code;
+
 typedef struct
 {
-    void *ctx;
-    uint8_t *(*read)(void *this, uint64_t offset, size_t count);
+    open_response_code (*open)(fmode_t mode);
+    int (*close)(void);
+    uint8_t *(*read)(uint64_t offset, size_t count);
     int (*write)(void *this, uint64_t offset, size_t count, uint8_t *data);
-} block_device_operations_t;
+} __attribute__((aligned(64))) block_device_operations_t;
 
-struct block_device {
+typedef struct block_device block_device;
+struct block_device
+{
+    uintptr_t                  bar;
     boolean_t                  used;
     void                      *identifier;
     block_device_operations_t *ops;
-    const char                *name;
-};
+    const char                 name[64];
+    struct block_device       *next;
+} __attribute__((aligned(64)));
 
-void                 block_install();
-void                 block_register_device(const char *name, block_device_operations_t *ops,
-                                           void *identifier);
-struct block_device *block_get_device(const char *name);
+void block_install();
+void block_register_device(const char *name, block_device_operations_t *ops, void *identifier);
+block_device *block_get_device(const char *name);
 
 #define BLOCK_OPT_NOT_IMPLEMENTED -1
 
