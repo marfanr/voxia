1e772ce401141373ba61166c8627a2d7111e6aec
marfanr <mohammadarfan45@gmail.com>
2025-11-19 21:43:12 +0700

    wp impl string

--- kernel/libk/string.c
+++ kernel/libk/string.c
@@ -32,3 +32,38 @@ stringcmp(string s1, string s2)
 
     return strncmp(s1->c_str, s2->c_str, s1->len) == 0;
 }
+
+string
+str_concat(string s, const char *suffix)
+{
+    size_t suffix_len = strlen(suffix);
+    size_t new_len    = s->len + suffix_len;
+
+    string s_new = (string)kalloc(sizeof(*s_new) + new_len + 1);
+    s_new->c_str = (char *)((uint8_t *)s_new + sizeof(*s_new));
+    s_new->len   = new_len;
+    s_new->cap   = new_len + 1;
+
+    strcpy(s_new->c_str, s->c_str);
+    strcpy(s_new->c_str + s->len, suffix);
+
+    return s_new;
+}
+
+void
+str_trim(string str)
+{
+    for (size_t i = str->len - 1; i >= 0; i--)
+    {
+        if (str->c_str[i] == ' ' || str->c_str[i] == '\n' || str->c_str[i] == '\t' ||
+            str->c_str[i] == '\r')
+        {
+            str->c_str[i] = '\0';
+            str->len -= 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
--- kernel/libk/string.h
+++ kernel/libk/string.h
@@ -1,6 +1,7 @@
 #ifndef __LIBK__STRING_H__
 #define __LIBK__STRING_H__
 
+#include "libk/vector.h"
 #include <libk/type.h>
 
 typedef struct
@@ -10,8 +11,11 @@ typedef struct
     uint32_t cap;
 } *string;
 
+define_vector(string);
 string    str(const char *str);
 void      str_release(string str);
 boolean_t stringcmp(string s1, string s2);
+string    str_concat(string s, const char *suffix);
+void      str_trim(string str);
 
 #endif // __LIBK__STRING_H__
