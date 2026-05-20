1b5a53fdad8b76f653df4150a2f19dcd7c98c966
marfanr <mohammadarfan45@gmail.com>
2026-05-21 05:57:41 +0700

    remove unused, duplicated

--- kernel/hal/graphic/virtio.h
+++
@@ -1,9 +0,0 @@
-#ifndef __HAL_GRAPHIC_VIRTIO_H__
-#define __HAL_GRAPHIC_VIRTIO_H__
-
-#include <ioforge/ioforge_pci.h>
-#include <type.h>
-
-void virtio_gpu_init(struct ioforge_pci_device* pci_device);
-
-#endif // __HAL_GRAPHIC_VIRTIO_H__
