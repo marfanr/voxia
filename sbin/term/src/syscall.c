2def93181937f37aa31a6640aa013f7502c2fce4
marfanr <mohammadarfan45@gmail.com>
2026-05-22 17:31:15 +0700

    remove unused graphic API and add syscall implementation for read/write

---
+++ sbin/term/src/syscall.c
@@ -0,0 +1,18 @@
+#include "term.h"
+
+__inline unsigned long __syscall3(long n, long a1, long a2, long a3) {
+	unsigned long ret;
+	__asm__ __volatile__("syscall"
+	                     : "=a"(ret)
+	                     : "a"(n), "D"(a1), "S"(a2), "d"(a3)
+	                     : "rcx", "r11", "memory");
+	return ret;
+}
+
+int read(int fd, void* buf, long count) {
+	return (int)__syscall3(0, fd, (long)buf, (long)count);
+}
+
+int write(int fd, void* buf, long count) {
+	return (int)__syscall3(1, fd, (long)buf, (long)count);
+}
