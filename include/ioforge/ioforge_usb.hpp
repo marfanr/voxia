ddd3a123b417ae42a2828bb3571f0ad7b163e0e5
marfanr <mohammadarfan45@gmail.com>
2026-05-22 15:19:03 +0700

    module: enable module E1000, Virtio GPU, EHCI

--- include/ioforge/ioforge_usb.hpp
+++ include/ioforge/ioforge_usb.hpp
@@ -7,36 +7,32 @@
 
 class IoForgeUSB : public IOForge {
       public:
-	IoForgeUSB(const char* mod) : IOForge(mod) {
-	}
+	IoForgeUSB(const char* mod) : IOForge(mod) {}
 	virtual void load();
 	virtual void unload();
-
-	inline static void send(uint32_t data, size_t size) {
-	}
 
 	template <typename Fn>
 	inline static void
 	foreach_usb_device_by_devclass(struct ioforge_device* node,
-				       uint16_t devclass, Fn&& fn) {
+	                               uint16_t devclass, Fn&& fn) {
 		// Bungkus lambda ke dalam void* ctx
 		ioforge_find_usb_device_by_devclass(
-			node, devclass,
-			[](struct ioforge_usb_device* dev, void* ctx) {
-				(*reinterpret_cast<Fn*>(ctx))(dev);
-			},
-			reinterpret_cast<void*>(&fn));
+		    node, devclass,
+		    [](struct ioforge_usb_device* dev, void* ctx) {
+			    (*reinterpret_cast<Fn*>(ctx))(dev);
+		    },
+		    reinterpret_cast<void*>(&fn));
 	};
 
 	inline static uint8_t
 	get_configuration(struct ioforge_usb_device* dev) {
 		uintptr_t setup_addr = 0;
 		struct usb_setup_packet* setup =
-			(struct usb_setup_packet*) IOUtils::DMAAlloc(
-				sizeof(struct usb_setup_packet), &setup_addr);
+		    (struct usb_setup_packet*)IOUtils::DMAAlloc(
+		        sizeof(struct usb_setup_packet), &setup_addr);
 
 		uintptr_t resp_paddr = 0;
-		uint8_t* resp = (uint8_t*) IOUtils::DMAAlloc(64, &resp_paddr);
+		uint8_t* resp = (uint8_t*)IOUtils::DMAAlloc(64, &resp_paddr);
 		IOUtils::memset(resp, 0, 64);
 
 		setup->bRequest = 0x08;
@@ -46,24 +42,25 @@ class IoForgeUSB : public IOForge {
 		setup->wLength = 1;
 
 		// todo: modify send method to add endpoint parameter
-		dev->controller->ops.send(dev->addr, 0, setup_addr,
-					  sizeof(*setup), resp_paddr, 1);
+		dev->controller->ops.send(dev->addr, 0, (uint32_t)setup_addr,
+		                          sizeof(*setup), (uint32_t)resp_paddr,
+		                          1);
 
 		uint8_t res = *resp;
 
-		IOUtils::DMAFree((void*) setup_addr, (void*) setup,
-				 sizeof(struct usb_setup_packet));
-		IOUtils::DMAFree((void*) resp_paddr, (void*) resp, 64);
+		IOUtils::DMAFree((void*)setup_addr, (void*)setup,
+		                 sizeof(struct usb_setup_packet));
+		IOUtils::DMAFree((void*)resp_paddr, (void*)resp, 64);
 
 		return res;
 	}
 
-	inline static void
-	set_configuration(struct ioforge_usb_device* dev, uint8_t val) {
+	inline static void set_configuration(struct ioforge_usb_device* dev,
+	                                     uint8_t val) {
 		uintptr_t setup_addr = 0;
 		struct usb_setup_packet* setup =
-			(struct usb_setup_packet*) IOUtils::DMAAlloc(
-				sizeof(struct usb_setup_packet), &setup_addr);
+		    (struct usb_setup_packet*)IOUtils::DMAAlloc(
+		        sizeof(struct usb_setup_packet), &setup_addr);
 
 		setup->bmRequestType = 0x0;
 		setup->bRequest = 0x09;
@@ -72,11 +69,11 @@ class IoForgeUSB : public IOForge {
 		setup->wLength = 0;
 
 		// todo: modify send method to add endpoint parameter
-		dev->controller->ops.send(dev->addr, 0, setup_addr,
-					  sizeof(struct usb_setup_packet), 0,
-					  0);
-		IOUtils::DMAFree((void*) setup_addr, (void*) setup,
-				 sizeof(*setup));
+		dev->controller->ops.send(dev->addr, 0, (uint32_t)setup_addr,
+		                          sizeof(struct usb_setup_packet), 0,
+		                          0);
+		IOUtils::DMAFree((void*)setup_addr, (void*)setup,
+		                 sizeof(*setup));
 	}
 };
 
