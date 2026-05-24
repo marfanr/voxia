47f14fb30c9dbcb6cd1a55c08139eb1b42f2823d
marfanr <mohammadarfan45@gmail.com>
2026-05-24 22:44:41 +0700

    hal: moved into kernel modules

--- kernel/sys/syscall/read.c
+++ kernel/sys/syscall/read.c
@@ -14,22 +14,22 @@ int syscall_read(int fd, void* buf, long count) {
 
     // TODO: sementara hardcode
     
-    serial2_printf("syscall read %d %x %d\n", fd, buf, count);
+    // serial2_printf("syscall read %d %x %d\n", fd, buf, count);
 
 
-    dentry_ptr out;
-    if (resolve_dentry("/dev/event/event0", 0, &out, 0) != VFS_OK) {
-        serial2_printf("failed resolve dentry\n");
-        return -1;
-    }
-    serial2_printf("dentry found: %s \n", out->name->c_str);
-    auto vnode = out->vnode;
-    auto priv = (struct dev_event_data *)vnode->vnode_private;
-
-    // for (size_t i = 0; i < 20; i++) {
-    //     serial2_printf("%x ", priv->data[i]);
+    // dentry_ptr out;
+    // if (resolve_dentry("/dev/event/event0", 0, &out, 0) != VFS_OK) {
+    //     serial2_printf("failed resolve dentry\n");
+    //     return -1;
     // }
-    serial2_printf("readed %d \n", priv->len);
+    // serial2_printf("dentry found: %s \n", out->name->c_str);
+    // auto vnode = out->vnode;
+    // auto priv = (struct dev_event_data *)vnode->vnode_private;
+
+    // // for (size_t i = 0; i < 20; i++) {
+    // //     serial2_printf("%x ", priv->data[i]);
+    // // }
+    // serial2_printf("readed %d \n", priv->len);
 
 
     return 0;
