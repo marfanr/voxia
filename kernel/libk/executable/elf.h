2dec93d2b2dce663568c7f3b8008c45fd49ac611
marfanr <mohammadarfan45@gmail.com>
2026-05-25 16:20:47 +0700

    sys: fix  wrong offset on elf relocate

--- kernel/libk/executable/elf.h
+++ kernel/libk/executable/elf.h
@@ -305,7 +305,9 @@ void elf_gnu_hash_parse(GnuHashHeader* gnu_hash, Elf64_Shdr* gnu_hash_sym,
 
 void elf_relocate_dyn(elf_dynamic_map* map, uintptr_t base,
                       GnuHashHeader* gnu_hash,
-                      symbols_ptr_vector_t* external_syms);
+                      symbols_ptr_vector_t* external_syms,
+                      struct elf_load_mmap_table* table, int table_count);
+					  
 void elf_get_symbol(const char* sym_name, uintptr_t base, elf_section_map* map,
                     uint8_t* data, symbols_ptr_vector_t* syms,
                     boolean_t skip_empty_val);
