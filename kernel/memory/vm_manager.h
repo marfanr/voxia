40eb61606a2125fc0149ff4c06432a332d8dc870
marfanr <mohammadarfan45@gmail.com>
2026-05-25 10:00:49 +0700

    vmm: change each vaddr region size

--- kernel/memory/vm_manager.h
+++ kernel/memory/vm_manager.h
@@ -4,20 +4,23 @@
 #include <spinlock.h>
 #include <type.h>
 
-enum mem_vma_base {
-	mem_vma_phys_window_start = 0xFFFFD00000000000,
-	mem_vma_phys_window_pt = 0xFFFFB0000F000000,
-};
-
-#define KERNEL_BASE  0xFFFF800000000000ULL
-#define REGION_SIZE  0x0000200000000000ULL  // 32 TB
-
+#define KERNEL_BASE 0xFFFF800000000000ULL
+#define REGION_SIZE 0x0000008000000000ULL // 512 GB (Bukan 32 TB)
+#define USER_STACK_VADDR 0x7FFFFFFFE000ULL
 
 typedef enum : uintptr_t {
-	VMA_REGION_A      = KERNEL_BASE,
-	VMA_REGION_B      = KERNEL_BASE + REGION_SIZE * 1,
-	VMA_REGION_C      = KERNEL_BASE + REGION_SIZE * 2,
-	VMA_REGION_KMODULE= KERNEL_BASE + REGION_SIZE * 3,
+	VMA_REGION_A = KERNEL_BASE,                     // 0xFFFF800000000000
+	VMA_REGION_B = KERNEL_BASE + (REGION_SIZE * 1), // 0xFFFF808000000000
+	VMA_REGION_C = KERNEL_BASE + (REGION_SIZE * 2), // 0xFFFF810000000000
+	VMA_REGION_KMODULE =
+	    KERNEL_BASE + (REGION_SIZE * 3), // 0xFFFF818000000000
+	KALLOC_BASE_ADDR =
+	    KERNEL_BASE + (REGION_SIZE * 4), // 0xFFFF820000000000
+
+	// Physical window bebas diletakkan jauh dari region di atas
+	mem_vma_phys_window_pt = 0xFFFFB00000000000ULL,
+	mem_vma_phys_window_start = 0xFFFFD00000000000ULL,
+
 	VMA_REGION_PROCESS = 0x400000,
 } mem_vma_region;
 
@@ -50,6 +53,6 @@ void vma_register(uintptr_t phys_address, uintptr_t virt_addr, size_t size);
 virtual_memory_t* vma_find(uintptr_t virt_addr);
 void vma_unregister(uintptr_t virt_addr);
 void vma_tree_add(mem_vma_region region, uintptr_t start_address,
-		  uintptr_t end_address);
+                  uintptr_t end_address);
 uintptr_t vma_lookup_free_vaddr(mem_vma_region region, size_t size);
 #endif // __MEMORY_VM_MANAGER_H__
