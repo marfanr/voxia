f11201e6c588398a2601c6be0c07b987aaffaee5
marfanr <mohammadarfan45@gmail.com>
2026-05-25 07:38:48 +0700

    graphic: move some method in console into graphic

--- kernel/hal/graphic/graphic.h
+++ kernel/hal/graphic/graphic.h
@@ -15,5 +15,10 @@ void put_pixel_alpha_fast(int x, int y, pixel_t src);
 uint32_t vxGetWidth(void);
 uint32_t vxGetHeight(void);
 void vxScroll(int px);
+void clear_screen(uint32_t color);
+uint32_t screen_cols(void);
+uint32_t screen_rows(void);
+
+#define FONT_SIZE 14
 
 #endif // __HAL__GRAPHIC__GRAPHIC_H__
