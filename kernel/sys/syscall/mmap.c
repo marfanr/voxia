f5c62820005728ff74f6cb9a8d1b48916019ff75
marfanr <mohammadarfan45@gmail.com>
2026-05-27 13:21:51 +0700

    refactor: decrease complexity on syscal_mmap

--- kernel/sys/syscall/mmap.c
+++ kernel/sys/syscall/mmap.c
@@ -46,80 +46,82 @@
 #define MAP_HUGE_2GB (31 << 26)
 #define MAP_HUGE_16GB (34U << 26)
 
+static uint64_t mmap_prot_to_flags(int prot) {
+	if (prot == PROT_NONE)
+		return 0;
+
+	uint64_t flags = 0;
+	if (prot & (PROT_READ | PROT_WRITE | PROT_EXEC))
+		flags |= PAGE_PRESENT;
+	if (prot & PROT_WRITE)
+		flags |= PAGE_WRITABLE;
+	if (!(prot & PROT_EXEC))
+		flags |= PAGE_NO_EXECUTE;
+
+	return flags;
+}
+
+static uintptr_t mmap_resolve_virt_addr(void* addr, int flags,
+                                        struct virtual_memory_page* vm_page,
+                                        size_t len_4kb) {
+	if (flags & MAP_FIXED) {
+		if (!addr)
+			return 0; /* MAP_FIXED + NULL → EINVAL */
+		return (uintptr_t)addr;
+	}
+
+	return vma_lookup_free_vaddr(vm_page, VMA_REGION_PROCESS, len_4kb);
+}
+
+static void* mmap_handle_anonymous(thread_t* thr, process_t* procc, void* addr,
+                                   int flags, size_t len_4kb,
+                                   uint64_t mmap_flags) {
+	serial2_printf("anonymous mmap: flags=0x%x\n", flags);
+
+	uintptr_t virt_addr =
+	    mmap_resolve_virt_addr(addr, flags, procc->vm_page, len_4kb);
+
+	if (!virt_addr)
+		return (flags & MAP_FIXED) ? (void*)-EINVAL : (void*)-ENOMEM;
+
+	uintptr_t phys = (uintptr_t)phys_base_alloc(len_4kb);
+	if (!phys)
+		return (void*)-ENOMEM;
+
+	vxMultipleMmap(thr->page, virt_addr, phys, len_4kb, mmap_flags);
+	serial2_printf("mmaped virt=0x%x phys=0x%x flags=%b\n", virt_addr, phys,
+	               mmap_flags);
+
+	vma_register(procc->vm_page, phys, virt_addr, len_4kb * BLOCK_SIZE);
+
+	return (void*)virt_addr;
+}
+
 void* syscall_mmap(void* addr, size_t len, int prot, int flags, int fd,
                    long off) {
 	auto thr = get_current_core_data()->active_thread;
-	auto procc = thr->process;
-	if (!thr || !procc) {
-		return (void*)-ENOENT;
-	}
+	auto procc = thr ? thr->process : nullptr;
 
-	serial_trace("mmap_request: addr 0x%x, len 0x%x, prot %d, flags "
-	             "0x%x, fd 0x%x, off 0x%x\n",
+	if (!thr || !procc)
+		return (void*)-ENOENT;
+
+	serial_trace("mmap_request: addr=0x%x len=0x%x prot=%d "
+	             "flags=0x%x fd=0x%x off=0x%x\n",
 	             addr, len, prot, flags, fd, off);
 
-	if (len == 0) {
+	if (len == 0)
 		return (void*)-EINVAL;
-	}
 
-	uint64_t mmap_flags = 0;
-	if (prot != PROT_NONE) {
-		if (prot & PROT_READ)
-			mmap_flags |= PAGE_PRESENT;
-		if (prot & PROT_WRITE)
-			mmap_flags |= PAGE_WRITABLE;
-		if (!(prot & PROT_EXEC))
-			mmap_flags |= PAGE_NO_EXECUTE;
+	size_t len_4kb = ALIGN_UP(len, 0x1000) / 0x1000;
+	uint64_t mmap_flags = mmap_prot_to_flags(prot);
 
-		if (prot & (PROT_READ | PROT_WRITE | PROT_EXEC))
-			mmap_flags |= PAGE_PRESENT;
-	}
+	if (flags & MAP_ANONYMOUS)
+		return mmap_handle_anonymous(thr, procc, addr, flags, len_4kb,
+		                             mmap_flags);
 
-	auto len_4kb = ALIGN_UP(len, 0x1000) / 0x1000;
+	if (flags & MAP_PRIVATE)
+		serial2_printf("mmap: MAP_PRIVATE not yet implemented\n");
 
-	if (flags & MAP_ANONYMOUS) {
-		serial2_printf("anonymous flag \n");
-
-		uintptr_t virt_addr = 0;
-
-		if (flags & MAP_FIXED) {
-			serial2_printf("fixed flag\n");
-			if (!addr) {
-				return (void*)-EINVAL; // MAP_FIXED with NULL is
-				                       // invalid
-			}
-			virt_addr = (uintptr_t)addr;
-		} else {
-			// find a free region in the process virtual address
-			// space
-			virt_addr = vma_lookup_free_vaddr(
-			    procc->vm_page, VMA_REGION_PROCESS, len_4kb);
-
-			if (!virt_addr) {
-				return (void*)-ENOMEM;
-			}
-		}
-
-		auto phys = phys_base_alloc(len_4kb);
-		if (!addr || !phys) {
-			return (void*)-EINVAL;
-		}
-
-
-		vxMultipleMmap(thr->page, (uint64_t)virt_addr, (uint64_t)phys,
-		               len_4kb, mmap_flags);
-		serial2_printf("mmaped 0x%x (%b)\n", virt_addr, mmap_flags);
-		
-		vma_register(procc->vm_page, (uintptr_t)phys, (uintptr_t)addr,
-		             len_4kb * BLOCK_SIZE);
-
-		return (void*)virt_addr;
-	}
-
-	if (flags & MAP_PRIVATE) {
-		serial2_printf("private flag\n");
-	}
-
-	serial2_printf("mmap: not supported\n");
+	serial2_printf("mmap: unsupported flags=0x%x\n", flags);
 	return (void*)-ENOSPC;
 }
