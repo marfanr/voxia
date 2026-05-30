f8be709b22e177a3bc4bebf951473226fa198745
marfanr <mohammadarfan45@gmail.com>
2026-05-30 15:24:00 +0700

    remove unused

--- root/sbin/term/include/term.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TERM_H__
-#define __TERM_H__
-
-int read(int fd, void* buf, long count);
-int write(int fd, void* buf, long count);
-int open(const char* path, int flags, int mode);
-
-#endif //
