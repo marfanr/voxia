1a667045465bc33cd53db6b51fb8b303a673c1af
marfanr <mohammadarfan45@gmail.com>
2026-06-10 20:07:01 +0700

    xhci: fix setup data passing by reading from physical memory

--- modules/xhci/src/init.cpp
+++ modules/xhci/src/init.cpp
@@ -1,15 +1,19 @@
 #include "ioforge/ioforge.h"
 #include "xhci/xhci.hpp"
 #include <ioforge/ioforge.hpp>
+#include <ioforge/ioforge_pci.h>
+#include <ioforge/ioforge_pci.hpp>
 #include <ioforge/ioforge_usb.h>
 
-IoForgeModuleConstructor(XHCIModule);
-
 XHCIModule XHCIModule::instance;
+
+extern "C" void load() { XHCIModule::getInstance()->load(); }
 
 XHCIModule::XHCIModule() : IOforgePCI("XHCI") {}
 
 XHCIModule* XHCIModule::getInstance() { return &instance; }
+
+extern "C" void xhci_fire_handler();
 
 void XHCIModule::load() {
 	device = findDevice(XHCI_VENDOR_ID, XHCI_DEVICE_ID);
@@ -29,16 +33,28 @@ void XHCIModule::load() {
 	runtime_regs = (struct xhci_runtime_regs*)(bar + cap_regs->rtsoff);
 	doorbell_regs = (volatile uint32_t*)(bar + cap_regs->dboff);
 
-	log(mod, "HCI version: %x", cap_regs->hciversion);
-
 	reset_controller();
 	init_controller();
 
-	// Enable interrupts if needed
-	if (device->interrupt_line) {
+	uint16_t msix_cap = pci_cap_find_msix(device);
+	uint16_t msi_cap = pci_cap_find_msi(device);
+
+	if (msix_cap) {
+		log(mod, "MSI-X Available at cap : 0x%x", msix_cap);
+		auto cpu = ioforge_get_current_core_id();
 		auto irq = IOUtils::irq_alloc_entry();
-		IOUtils::isr_map(device->interrupt_line, irq);
-		IOUtils::irq_register(irq, (void*)XHCIModule::fireHandler);
+		IOUtils::irq_register(irq, (void*)xhci_fire_handler);
+		pci_enable_msix(device, irq, cpu, msix_cap);
+	} else if (msi_cap) {
+		log(mod, "MSI Available at 0x%x", msi_cap);
+		auto cpu = ioforge_get_current_core_id();
+		auto irq = IOUtils::irq_alloc_entry();
+		IOUtils::irq_register(irq, (void*)xhci_fire_handler);
+		pci_enable_msi(device, irq, cpu, msi_cap);
+	} else if (device->interrupt_line) {
+		auto vector = isr_irq_register(device->interrupt_line,
+		                               (void*)xhci_fire_handler);
+		// Note: no controller->irq = vector here as XHCI doesn't store it yet
 	}
 
 	probe_ports();
@@ -47,11 +63,17 @@ void XHCIModule::load() {
 }
 
 extern "C" void xhci_send_async_stub(uint32_t addr, uint8_t endpoint,
-                                     uint32_t data_phys, size_t request_size,
-                                     uint32_t response_phys,
+                                     uintptr_t data_phys, size_t request_size,
+                                     uintptr_t response_phys,
                                      size_t response_size) {
+	// data_phys is a physical address to the 8-byte setup packet.
+	// We must map it to read the actual data.
+	uintptr_t vaddr = IOforgeMMapPhys(data_phys, 8);
+	uint64_t setup_data = *(uint64_t*)vaddr;
+	IOforgeMUnmapPhys(vaddr, 8);
+
 	XHCIModule::getInstance()->send_async_with_response(
-	    addr, endpoint, data_phys, request_size, response_phys,
+	    addr, endpoint, setup_data, request_size, response_phys,
 	    response_size);
 }
 
@@ -67,8 +89,4 @@ __attribute__((constructor)) static void xhci_constructor() {
 
 	XHCIModule::getInstance()->set_controller(usb_controller);
 	ioforge_attach(ioforge_get_usb_ctrl_root(), &usb_controller->service);
-}
-
-void XHCIModule::fireHandler() {
-	// Handle interrupts
 }
