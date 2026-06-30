58a635881b50661be26e0e87457222d0e157755c
marfanr <mohammadarfan45@gmail.com>
2026-06-30 13:43:00 +0700

    feat: update PHYS2VIRT and VIRT2PHYS macros to use g_hhdm_offset

--- include/memory/memory_utils.h
+++ include/memory/memory_utils.h
@@ -8,8 +8,12 @@
 
 #define ALIGN_DOWN(x, align) ((x) & ~((uintptr_t) (align) - 1))
 
-#define PHYS2VIRT(x) (uint64_t)((uint64_t) x + 0xffff800000000000UL)
-#define VIRT2PHYS(x) (uint64_t)((uint64_t) x - 0xffff800000000000UL)
+extern uint64_t g_hhdm_offset;
+/* WARNING: These macros depend on a global HHDM mapping which has been removed. 
+   Only use during early boot or for physical address calculations from bootloader pointers. 
+   After paging is set, use physical windows for accessing arbitrary physical memory. */
+#define PHYS2VIRT(x) (uint64_t)((uint64_t)(x) + g_hhdm_offset)
+#define VIRT2PHYS(x) (uint64_t)((uint64_t)(x) - g_hhdm_offset)
 
 #define PTR_ADD(ptr, off) ((void*) (((uint8_t*) (ptr)) + (off)))
 
