95f56612c1fd6df2ff5ad437fd0a6c0320e3f43d
marfanr <mohammadarfan45@gmail.com>
2026-07-23 23:29:00 +0700

    kernel: Add /dev/urandom and update init sequence

--- kernel/hal/rand/rand.c
+++ kernel/hal/rand/rand.c
@@ -3,16 +3,46 @@
 #include "hal/cpu/cpuid.h"
 #include "init/init.h"
 #include "libk/serial.h"
-
-static boolean_t random_available = 0;
+#include "str.h"
+#include "vfs/dentry.h"
+#include "vfs/enum.h"
+#include "vfs/vfs.h"
+#include "vfs/vnode.h"
 
 #define RAND_MAX 0xFFFFFFFF
+
+static boolean_t random_available = 0;
+static dentry_ptr urandom_dentry = 0;
+
+static int urandom_read(vnode_t* vnode, void* buf, size_t len, size_t offset)
+{
+	(void)vnode;
+	(void)offset;
+	(void)buf;
+	(void)len;
+
+	uint32_t random = vxRand();
+	memcopy(buf, &random, sizeof(uint32_t));
+	return 0;
+}
+
+static vops_file_t urandom_ops = {
+	.read = urandom_read,
+};
 
 INIT(Rand) {
 	uint32_t ecx, unused;
 	cpuid(1, 0, &unused, &unused, &ecx, &unused);
 	random_available = (ecx >> 30) & 1;
 	LOG_INFO("Rand ", "random is available %d", random_available);
+	if (vxnamei("/dev/urandom", &urandom_dentry) != VFS_OK) {
+		return;
+	}
+
+	urandom_dentry->vnode = create_and_attach_vnode();
+	urandom_dentry->vnode->type = VNODE_TYPE_FILE;
+	urandom_dentry->vnode->permission = 0444;
+	urandom_dentry->vnode->ops = &urandom_ops;
 }
 
 uint32_t vxRand() {
